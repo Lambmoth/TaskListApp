31da1fa0b74a409b4d4b216e486e2e34802433a8
gjlandrews <gja@ualberta.ca>
2020-11-20 19:14:12 -0700

    tasks can now be deleted, checked and filtered

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 const taskInput = document.querySelector('.task-input');
 const taskButton = document.querySelector('.task-button');
 const taskList = document.querySelector('.task-list');
-
+const filterOption = document.querySelector('.filter-tasks');
 //Event Listeners
-    taskButton.addEventListener('click', addTask);
+taskButton.addEventListener('click', addTask);
+taskList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTasks);
 
 //Functions
 
@@ -34,4 +36,53 @@ function addTask(event){
     taskList.appendChild(taskDiv);
     //Clear task input text
     taskInput.value = "";
+}
+
+function deleteCheck(event){
+    //console.log(event.target)
+    const item = event.target;
+    //DELETE TASK
+    if(item.classList[0] === "trash-btn"){
+        const task = item.parentElement;
+        //Animate delete
+        task.classList.add("fall");
+        task.addEventListener('transitionend', function(){
+            task.remove();
+        });
+    }
+
+    //Check mark
+    if(item.classList[0] === "complete-btn") {
+        const task = item.parentElement;
+        task.classList.toggle("completed");
+    }
+}
+
+function filterTasks(event){
+    const tasks = taskList.childNodes;
+    // console.log(tasks);
+    tasks.forEach(function(task){
+        switch(event.target.value){
+            case "all":
+                task.style.display = 'flex';
+                break;
+            case "completed":
+                if(task.classList.contains('completed')){
+                    task.style.display = 'flex';
+                }
+                else{
+                    task.style.display = 'none';
+                }
+                break;
+            case "not-completed":
+                if(task.classList.contains('not-completed')){
+                    task.style.display = 'flex';
+                }
+                else{
+                    task.style.display = 'none';
+                }
+                break;
+
+        }
+    });
 }
